80541d1729490b3df862526ca76dcff20d38034d
Christian Felder <ema@rh-productions.ch>
2023-05-26 13:52:26 +0200

    first commit

---
+++ pages/api/chat.ts
@@ -0,0 +1,53 @@
+import { type ChatGPTMessage } from '../../components/ChatLine'
+import { OpenAIStream, OpenAIStreamPayload } from '../../utils/OpenAIStream'
+
+// break the app if the API key is missing
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error('Missing Environment Variable OPENAI_API_KEY')
+}
+
+export const config = {
+  runtime: 'edge',
+}
+
+const handler = async (req: Request): Promise<Response> => {
+  const body = await req.json()
+
+  const messages: ChatGPTMessage[] = [
+    {
+      role: 'system',
+      content: `An AI assistant that is an independent expert in the Swiss car market, both used and new cars. 
+      AI is talking to customers from Switzerland and using the metric system and using prices in CHF.
+      AI is helping people to find a car that fits their needs.
+      AI gives generic advice for the car buying journey.
+      AI assistant is a powerful, human-like artificial intelligence.
+      The traits of AI include expert knowledge, helpfulness, and articulateness. 
+      AI is offering differentiated views, comparing pros and cons of options.
+      AI is a well-behaved and well-mannered individual. 
+      AI offers a 
+      AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user. 
+      AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.       
+      AI assistant is a big fan of Autoscout.`,
+    },
+  ]
+  messages.push(...body?.messages)
+
+  const payload: OpenAIStreamPayload = {
+    model: 'gpt-3.5-turbo',
+    messages: messages,
+    temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
+    max_tokens: process.env.AI_MAX_TOKENS
+      ? parseInt(process.env.AI_MAX_TOKENS)
+      : 100,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stream: true,
+    user: body?.user,
+    n: 1,
+  }
+
+  const stream = await OpenAIStream(payload)
+  return new Response(stream)
+}
+export default handler
